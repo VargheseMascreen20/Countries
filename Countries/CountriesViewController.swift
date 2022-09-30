c244bf28562cd01d8a5426cb972d3fea66c0e2ad
gilshaan <gilshaanmj@gmail.com>
2022-09-30 10:40:14 +0530

    Initial Commit

---
+++ Countries/CountriesViewController.swift
@@ -0,0 +1,44 @@
+//
+//  CountriesViewController.swift
+//  Countries
+//
+//  Created by DDUKK7 on 29/09/22.
+//
+
+import UIKit
+
+struct Country {
+    var isoCode: String
+    var name: String
+}
+
+class CountriesTableViewController: UITableViewController {
+    let countries = [
+        Country(isoCode: "at", name: "Austria"),
+        Country(isoCode: "be", name: "Belgium"),
+        Country(isoCode: "de", name: "Germany"),
+        Country(isoCode: "el", name: "Greece"),
+        Country(isoCode: "fr", name: "France"),
+    ]
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.countries.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CountryCell", for: indexPath)
+
+        let country = self.countries[indexPath.row]
+        cell.textLabel?.text = country.name
+        cell.detailTextLabel?.text = country.isoCode
+        cell.imageView?.image = UIImage(named: country.isoCode)
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        "Countries"
+    }
+}
---
+++ Countries/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Countries
+//
+//  Created by DDUKK7 on 29/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
